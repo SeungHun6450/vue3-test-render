b116d2c5510b15237601c5aed8cc8203c5c644ea
SeungHun6450 <toffg6450@naver.com>
2022-05-06 14:20:01 +0900

    vue-webpack-render

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div>
+    4보다 큽니다!
+  </div>
+  <ul>
+    <Fruit 
+      v-for="fruit in fruits"
+      :key="fruit"
+      :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from '~/components/Fruit'
+
+export default {
+  components: {
+    // Fruit: Fruit, 이름이 같으면 생략이 가능하다
+    Fruit
+  },
+  data() {
+    return {
+      // 반응성(Reactivity): 데이터를 갱신하면 화면도 바뀐다.
+      count: 0,
+      fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    font-size: 50px;
+    color: royalblue;
+  }
+  ul{
+    li {
+      font-size: 40px;
+    }
+  }
+</style>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+// scoped: 유효한 범위가 현재 컴포넌트다
+  h1{
+    color: red !important;
+  }
+</style>
